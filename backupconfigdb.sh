abc0e8a094d52955328562d8adf60a15954f2735
Thorsten Behrens <tbehrens@outlook.com>
2020-03-20 16:48:36 -0400

    Add files via upload

---
+++ backupconfigdb.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This file backs up your FreeNAS configuration database and keeps it for N days
+#
+# Location to store backup files in, adjust this to your dataset and path
+# Note this path has to end with '/'
+BackupDir=/mnt/Gion/Backup/FreeNAS-config/
+# Number of days to keep backups for
+KeepFor=60
+#
+# /etc/version in format 'FreeNAS-11.3-RELEASE (c0e049a7fa)'; use cut to extract 11.3_RELEASE 
+cp /data/freenas-v1.db $BackupDir`date +%Y%m%d`_`cat /etc/version | cut -d'-' -f2`_`cat /etc/version | cut -d'-' -f3 | cut -d' ' -f1`.db
+find $BackupDir -type f -depth 1 -mtime +$KeepFor -delete
---
+++ checkconfigdb.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+# This file runs a database integrity check and emails you in the event that
+# your database is corrupt. This file requires you to have *properly* setup
+# emailing from the FreeNAS GUI.  The following variables are available for
+# you to use:
+#
+# YourEmail:  The email address you want to send the email to if your database
+# is found to be corrupt.  Multiple email addresses are supported if separated
+# by a space.
+YourEmail="tbehrens@outlook.com"
+# ServerName:  How you want your server's name to appear in the email in the
+# event that the database is corrupt.
+ServerName=FreeNAS
+# TempLocation:  Location for the temp file for the email.  Default is /tmp
+TempLocation=/tmp
+# Scroll down to edit the email as you see fit.  The default setup is recommended
+# since it works and conveys a simple email to let you know what the problem
+# is and that you need to take action.
+# If you want to test this to ensure it works, simply rename the line with
+# freenas-v1.db to pointto a file that doesn't exist.  It will error, and you will get
+# an email.
+if [ -f "$TempLocation"/badconfig.txt ];
+    then rm "$TempLocation"/badconfig.txt
+fi
+ 
+if [ "$( sqlite3 /data/freenas-v1.db "pragma integrity_check;" )" == "ok" ]; then
+        #echo "Database is ok."
+        #bail out with zero (all o.k.) status
+        exit 0
+    else
+        # The following lines are the email that will be sent in the event that errors
+        # are found.  Change it however you wish, just make sure the general format
+        # is protected.
+        echo "To: $YourEmail" >> $TempLocation/badconfig.txt
+        echo "Subject: ERROR: Database corrupt on server $ServerName" >> $TempLocation/badconfig.txt
+        echo "Your server, $ServerName, has been found to have a corrupt FreeNAS config." >> $TempLocation/badconfig.txt
+        echo " " >> $TempLocation/badconfig.txt
+        echo "It is recommended you troubleshoot and correct the problem as soon as possible.  Just because the server is operating fine does not mean you can ignore this message." >> $TempLocation/badconfig.txt
+        echo "$TempLocation"/badconfig.txt
+        sendmail -t < "$TempLocation"/badconfig.txt
+        rm "$TempLocation"/badconfig.txt
+    fi
+exit 1
